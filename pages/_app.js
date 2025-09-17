b89d816083e4a810d494a9ef8b12474451ead1b9
salamonszilard <salamonszilard@gmail.com>
2025-09-17 10:55:05 +0200

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import { Fragment, useEffect, useState } from "react";
+import PreLoader from "../src/layouts/PreLoader";
+import "../styles/globals.css";
+const App = ({ Component, pageProps }) => {
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 500);
+  }, []);
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Luique - Personal Portfolio React NextJS Template</title>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="HandheldFriendly" content="true" />
+        <meta name="author" content="bslthemes" />
+        {/* Favicon */}
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      {loader && <PreLoader />}
+      <Component {...pageProps} />
+    </Fragment>
+  );
+};
+export default App;
